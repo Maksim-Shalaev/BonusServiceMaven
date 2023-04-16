68ad44627233d91872511fd3643cbb7b05f31838
Maksim Shalaev <maksimshalaev11@gmail.com>
2023-04-16 14:52:15 +0300

    Changing variables

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -8,45 +8,45 @@ public class BonusServiceTest {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 1_000_000;
+        long amount = 30_000;
         boolean registered = true;
         long expected = 500;
 
         // вызываем целевой метод:
-        long actual = service.calculate(500, true);
+        long actual = service.calculate(amount, registered);
 
         // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(500, 500);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void shouldCalculateForRegistredAndUnderLimit() {
         BonusService service = new BonusService();
-        long amount = 1_000;
+        long amount = 10_000;
         boolean registred = true;
-        long expected = 30;
-        long actual = service.calculate(1_000, true);
-        Assertions.assertEquals(30, 30);
+        long expected = 300;
+        long actual = service.calculate(amount, registred);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void shouldCalculateForUnregistredAndOverLimit() {
         BonusService service = new BonusService();
-        long amount = 1_000_000;
+        long amount = 650_000;
         boolean registred = false;
         long expected = 500;
-        long actual = service.calculate(1_000_000, false);
-        Assertions.assertEquals(500, 500);
+        long actual = service.calculate(amount, registred);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void shouldCalculateForUnregistredAndUnderLimit() {
         BonusService service = new BonusService();
-        long amount = 1_000;
+        long amount = 12_000;
         boolean registred = false;
-        long expected = 10;
-        long actual = service.calculate(1_000, false);
-        Assertions.assertEquals(10, 10);
+        long expected = 120;
+        long actual = service.calculate(amount, registred);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
